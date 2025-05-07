eb6199f672fffa897c9605fbcef9c9ef1c951797
TrilhaX <joaotrilha01@gmail.com>
2025-05-07 10:06:44 -0300

    Adicionado pegar o logs do txt pelo ID

--- main.js
+++ main.js
@@ -5,27 +5,25 @@ const dados = []
 const app = express()
 const PORT = 8000
 
-function writeFile(content) {
+
+function writeFile(obj) {
     try {
-        fs.writeFileSync('logs.txt', content, { flag: 'a' })
-        fs.readFileSync('logs.txt', 'utf-8', (err, data) => {
-            console.log(data)
-        })
+        fs.appendFileSync('logs.txt', JSON.stringify(obj) + '\n');
     } catch {
-        console.error("Erro")
+        console.error("Erro ao escrever no arquivo");
     }
 }
 
 function createNewDados(name) {
     let id = uuidv4()
-    const data = new Date().toISOString().replace("T", " ").slice(0, 19)
+    const data = new Date().toISOString().replace("T", "-").slice(0, 19)
     const nome = name
     try {
         const conteudo = fs.readFileSync('logs.txt', 'utf-8');
-        if (conteudo.includes(id)){
+        if (conteudo.includes(id)) {
             return
         }
-    }catch (err){
+    } catch (err) {
         console.error(err)
     }
     const novosDados = {
@@ -37,10 +35,22 @@ function createNewDados(name) {
     writeFile(JSON.stringify(dados))
 }
 
-app.get('/logs', (req,res) => {
+app.get('/logs', (req, res) => {
     res.status(200).send(dados)
     console.log("dados pegos:\n", dados)
 })
+
+app.get('/logs/:id', (req, res) => {
+    const { id } = req.params;
+    const dado = dados.find(item => item.ID === id);
+    if (dado) {
+        return res.status(200).json(dado);
+    } else {
+        return res.status(404).send('Usuário não encontrado');
+        
+    }
+});
+
 
 app.post('/logs', (req, res) => {
     dado = createNewDados("Joao")
