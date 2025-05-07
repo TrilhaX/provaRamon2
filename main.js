6d66351021751ddba604283bda5afcd662ae792a
TrilhaX <joaotrilha01@gmail.com>
2025-05-07 09:32:10 -0300

    Adicionado verificação de ID igual

--- main.js
+++ main.js
@@ -20,15 +20,25 @@ function createNewDados(name) {
     let id = uuidv4()
     const data = new Date().toISOString().replace("T", " ").slice(0, 19)
     const nome = name
-    dados.push({
+    try {
+        const conteudo = fs.readFileSync('logs.txt', 'utf-8');
+        if (conteudo.includes(id)){
+            return
+        }
+    }catch (err){
+        console.error(err)
+    }
+    const novosDados = {
         ID: id,
         Data: data,
         Nome: nome
-    })
+    }
+    dados.push(novosDados)
     writeFile(JSON.stringify(dados))
 }
+createNewDados("Joao")
 
-createNewDados("João")
+app.get()
 
 app.listen(PORT, () => {
     console.log("Servidor online")
