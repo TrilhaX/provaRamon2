98ca20dece32dcff57d9df91a8ee24af7e9acbf7
TrilhaX <joaotrilha01@gmail.com>
2025-05-07 09:01:52 -0300

    Adicionado a função de criar os dados e tambem a função de escrever eles no txt sem apagar o dado anterior

---
+++ main.js
@@ -0,0 +1,28 @@
+const { error } = require('console')
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
+const dados = []
+
+function writeFile(content){
+    try {
+        fs.writeFileSync('logs.txt', content, {flag: 'a'})
+        fs.readFileSync('logs.txt', 'utf-8', (err,data) => {
+            console.log(data)
+        })
+    }catch{
+        console.error("Erro")
+    }
+}
+
+function createNewDados(name){
+    let id = uuidv4()
+    const data = Date.now();
+    const nome = name
+    dados.push({
+        ID: id,
+        Data: data,
+        Nome: nome
+    })
+    writeFile(JSON.stringify(dados))
+}
+createNewDados("João")
